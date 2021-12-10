9e7f4e414f0b027fdd12ffae8aa5919065555736
Zachary Bickmann <zbickmann@oakland.edu>
2021-12-10 15:12:55 -0500

    initial heroku commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true}));
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/displayPage", (req, res) => {
+    res.render("displayPage");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/newEntry", (req, res) => {
+    res.render("newEntry");
+});
+
+app.get("/signUp", (req, res) => {
+    res.render("signUp");
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Server is running on ${port}'));
