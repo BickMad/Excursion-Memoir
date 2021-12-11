b8e2b7a6bd4fd05d065cb6ef46d670babaf00921
Zachary Bickmann <zbickmann@oakland.edu>
2021-12-11 16:50:27 -0500

    added some stuff

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 const express = require("express");
 const ejs = require("ejs");
 var mysql = require("mysql");
-var session = require("express-session");
-var bodyParser = require("body-parser");
-var path = require("path");
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 var connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
-	password : '',
+	password : 'Halothane2214$',
 	database : 'nodelogin'
 });
 
 const app = express();
 
-app.use(session({
-	secret: 'secret',
-	resave: true,
-	saveUninitialized: true
-}));
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json())
 
 app.set("view engine", "ejs");
+
 
 
 app.use("/public", express.static(__dirname + "/public"));
@@ -45,25 +40,16 @@ app.get("/newEntry", (req, res) => {
 app.get("/login", (req, res) => {
     res.render("login");
 });
+app.post('/login', (req, res) =>{
 
-app.post('/login', function(request, response) {
-	var username = request.body.username;
-	var password = request.body.password;
-	if (username && password) {
-		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-			if (results.length > 0) {
-				request.session.loggedin = true;
-				request.session.username = username;
-				response.redirect('/home');
-			} else {
-				response.send('Incorrect Username and/or Password!');
-			}			
-			response.end();
-		});
-	} else {
-		response.send('Please enter Username and Password!');
-		response.end();
-	}
+});
+
+
+app.get("/signUp", (req, res) => {
+    res.render("signUp");
+});
+app.post('/signUp', (req, res) =>{
+    console.log(req.body);
 });
 
 
