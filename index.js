5fff4dac826166d0ac1ab64a2d8cc5c44d1e7b64
Zachary Bickmann <zbickmann@oakland.edu>
2021-12-11 15:43:09 -0500

    hello

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const express = require("express");
 const ejs = require("ejs");
+var mysql = require("mysql");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'nodelogin'
+});
 
 const app = express();
 
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json())
+
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true}));
+
 
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
     res.render("index");
+    if (request.session.loggedin) {
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
 });
 
 app.get("/displayPage", (req, res) => {
@@ -26,14 +51,24 @@ app.get("/newEntry", (req, res) => {
 app.get("/login", (req, res) => {
     res.render("login");
 });
-
-
-
-app.get("/signUp", (req, res) => {
-    res.render("signUp");
-});
-app.post('/signUp', (req, res) =>{
-
+app.post('/login', function(request, response) {
+	var username = request.body.Username;
+	var password = request.body.Password;
+	if (username && password) {
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
 });
 
 
