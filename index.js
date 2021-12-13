c48fbc2d13e843e2d26c33a3822add9e29f39b4f
Zachary Bickmann <zbickmann@oakland.edu>
2021-12-13 11:56:49 -0500

    testing to see if database works on heroku

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+
 const express = require("express");
 const ejs = require("ejs");
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const { Prohairesis } = require("prohairesis");
+const env = require('./env');
 
-const mySQLString = '//be9a5bccf971a1:0a408364@us-cdbr-east-05.cleardb.net/heroku_c50927f03878efc?reconnect=true';
-const database = new Prohairesis(mySQLString);
+const database = new Prohairesis(env.CLEARDB_DATABASE_URL);
+
 
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json())
 
 app.set("view engine", "ejs");
@@ -47,12 +49,12 @@ app.get("/signUp", (req, res) => {
 });
 app.post('/signUp', async (req, res) =>{
     const body = req.body;
-    await database.execute('INSERT INTO user (Username, Password) VALUES(@Username, @Password, NOW())', {
+    await database.execute('INSERT INTO user (Username, Password) VALUES (@Username, @Password)', {
         Username: body.Username,
         Password: body.Username
     })
     res.send('added user');
-})
+});
 
 
 
